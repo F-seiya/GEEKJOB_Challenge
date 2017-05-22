6be459fd4aba1e2b6886cd2295328fc965c69fc7
藤川誠也 <s.f.the7h.of.hep@gmail.com>
2017-05-22 12:45:53 +0900

    [Java クラスの継承]  の課題提出します。

---
+++ CampChallenge_java/006.オブジェクト指向２/class02.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.PrintWriter;
+
+/**
+ *
+ * @author seiya
+ */
+public class class02 extends class01 {
+    
+        public String name ="";
+        
+        public int age = 0;
+    
+        public void setHuman(String n,int a){
+            
+            this.name = n;
+            this.age = a;
+          }
+        
+        public void sample(PrintWriter pw){
+            pw.print(name);
+            pw.print(age);
+        }
+   
+    
+    }
+
+    
+ 
