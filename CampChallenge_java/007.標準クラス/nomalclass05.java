5a0c9c8d986907219ab0bb4f1cd891fa812e5012
藤川誠也 <s.f.the7h.of.hep@gmail.com>
2017-05-25 11:15:14 +0900

    [Java バイト数と文字数の取得] の課題提出します。

---
+++ CampChallenge_java/007.標準クラス/nomalclass05.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author seiya
+ */
+public class nomalclass05 {
+    
+    public static void main(String[] args){
+        
+        String name  = "藤川誠也";
+        
+        System.out.println(name.length());
+    
+        System.out.print(name.getBytes().length);
+    }
+    
+}
