3b403f9bfb455e77faa5835732eebc4401d6290a
藤川誠也 <s.f.the7h.of.hep@gmail.com>
2017-05-25 11:16:05 +0900

    [Java 文字数の取得] の課題提出します。

---
+++ CampChallenge_java/007.標準クラス/nomalclass06.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+/**
+ *
+ * @author seiya
+ */
+public class nomalclass06 {
+    
+    public static void main(String[] args){
+    
+        String address = "mail@docomo.ne.jp"; 
+    
+        System.out.print(address.substring(4));
+        
+        
+    }
+    
+}
