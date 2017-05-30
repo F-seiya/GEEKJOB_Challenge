4ee31be560e041217ea4c88adeb567ed72ccbac2
藤川誠也 <s.f.the7h.of.hep@gmail.com>
2017-05-30 19:07:23 +0900

    [Java /データ操作  クッキーの記録と表示] の課題提出します。

---
+++ CampChallenge_java/008.データ操作/datasousa06.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet02;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import javax.servlet.http.Cookie;
+/**
+ *
+ * @author seiya
+ */
+public class datasousa06 extends HttpServlet {
+    
+    
+    /**
+     * 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        
+        Date d = new Date();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        
+        //Cookie作成
+        Cookie co = new Cookie("Login",sdf.format(d) );
+        
+        
+        
+        //Cookieに反映
+        response.addCookie(co);
+           
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+         //Cookieの取得  
+        Cookie[] cookie = request.getCookies();   
+          if(cookie != null){
+              for(int i =0; i<cookie.length; i++){
+                  if(cookie[i].getName().equals("Login")){
+                      out.print("最後のログインは、" + cookie[i].getValue());
+                      
+                  }else{
+                      out.print("最初のログインです。");
+                  }
+                      
+              }
+          }
+        
+          
+            
+        }
+        
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
